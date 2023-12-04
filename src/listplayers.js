4e1e78ceff280ed3a310ad244b351baec16d078f
Pol Sañé Jové <pol.sane@uvic.cat>
2023-12-04 10:22:55 +0000

    changed so that the list and details are no longer able to be seen together

--- src/listplayers.js
+++ src/listplayers.js
@@ -10,47 +10,60 @@ function ListPlayers() {
 
     const [players, setPlayers] = useState(initialPlayers);
 
-
+    const [showPlayerDatails, setShowPlayersDetails] = useState(false)
 
     const handleMoreClick = (playerId) => {
-            setSelectedPlayerId(playerId);
+            setSelectedPlayerId(playerId)
+            setShowPlayersDetails(true)
     };
 
     const handleDeleteClick = (playerId) => {
             const updatedPlayers = players.filter(player => !(player.id===playerId));
             setPlayers(updatedPlayers)
     };
+    
+    const handleBackToListClick = () => {
+            setShowPlayersDetails(false)
+    };
 
     return (
         <section className="w-1140px"> 
-            <h2>Top Soccer Players</h2> 
-            <table className="data-table">
-                <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Name</th>
-                        <th>Trophies</th>
-                        <th>Club</th>
-                        <th>Nationality</th>
-                        <th>Details</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {players.map(player => (
-                        <tr key={player.id}>
-                            <td>{player.id}</td>
-                            <td>{player.firstname} {player.lastname} ({player.age})</td>
-                            <td>{player.trophies}</td>
-                            <td>{player.club}</td>
-                            <td>{player.nationality}</td>
-                            <td><button onClick = {() => handleMoreClick(player.id)}>More...</button></td>
-                            <td><button className='delete' onClick = {() => handleDeleteClick(player.id)}>Delete</button></td>
+            {!showPlayerDatails ? (
+            <>    
+                <h2>Top Soccer Players</h2> 
+                <table className="data-table">
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Name</th>
+                            <th>Trophies</th>
+                            <th>Club</th>
+                            <th>Nationality</th>
+                            <th>Details</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            {selectedPlayerId && <DisplayPlayer playerId={selectedPlayerId}/>}
+                    </thead>
+                    <tbody>
+                        {players.map(player => (
+                            <tr key={player.id}>
+                                <td>{player.id}</td>
+                                <td>{player.firstname} {player.lastname} ({player.age})</td>
+                                <td>{player.trophies}</td>
+                                <td>{player.club}</td>
+                                <td>{player.nationality}</td>
+                                <td><button onClick = {() => handleMoreClick(player.id)}>More...</button></td>
+                                <td><button className='delete' onClick = {() => handleDeleteClick(player.id)}>Delete</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </>
+            ) : (
+                <>
+                    {<DisplayPlayer playerId={selectedPlayerId}/>}
+                    <button onClick={handleBackToListClick} className='back-to-list'>Back to list.</button>
+                </>
+            )}
         </section>
     );
 }
